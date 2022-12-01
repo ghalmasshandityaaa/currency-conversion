d80a74f6fab29af464bc9f3b380abb605c5d0cde
ghalmasshandityaaa <ghalmasshandityaaa@gmail.com>
2022-12-01 16:03:12 +0700

    feat: Add currency from on result conversion

--- src/Main.java
+++ src/Main.java
@@ -43,28 +43,28 @@ public class Main {
 
         switch(choice) {
             case 1:
-                System.out.printf("Result : %.2f YEN%n", yen.toYen(amount));
+                System.out.printf("Result : %.2f IDR = %.2f YEN%n", amount, yen.toYen(amount));
                 break;
             case 2:
-                System.out.printf("Result : %.2f IDR%n", yen.fromYen(amount));
+                System.out.printf("Result : %.2f YEN = %.2f IDR%n", amount, yen.fromYen(amount));
                 break;
             case 3:
-                System.out.printf("Result : %.2f RINGGIT%n", ringgit.toRinggit(amount));
+                System.out.printf("Result : %.2f IDR = %.2f RINGGIT%n", amount, ringgit.toRinggit(amount));
                 break;
             case 4:
-                System.out.printf("Result : %.2f IDR%n", ringgit.fromRinggit(amount));
+                System.out.printf("Result : %.2f RINGGIT = %.2f IDR%n", amount, ringgit.fromRinggit(amount));
                 break;
             case 5:
-                System.out.printf("Result : %.2f SGD%n", sgd.toSgd(amount));
+                System.out.printf("Result : %.2f IDR = %.2f SGD%n", amount, sgd.toSgd(amount));
                 break;
             case 6:
-                System.out.printf("Result : %.2f IDR%n", sgd.fromSgd(amount));
+                System.out.printf("Result : %.2f SGD = %.2f IDR%n", amount, sgd.fromSgd(amount));
                 break;
             case 7:
-                System.out.printf("Result : %.2f YUAN%n", yuan.toYuan(amount));
+                System.out.printf("Result : %.2f IDR = %.2f YUAN%n", amount, yuan.toYuan(amount));
                 break;
             case 8:
-                System.out.printf("Result : %.2f IDR%n", yuan.fromYuan(amount));
+                System.out.printf("Result : %.2f YUAN = %.2f IDR%n", amount, yuan.fromYuan(amount));
                 break;
             default:
                 System.out.println("Thank you for using the currency converter app");
